531e3d8e5249eeab468631d65856ebd87cc93445
Adam Youssef <adam@yucef.ca>
2023-12-10 15:53:10 +0200

    Integrate with Firebase

--- lib/app/app_firebase.dart
+++ lib/app/app_firebase.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:weight_tracker/firebase_options.dart';
 
 abstract class AppFirebase {
   static Future initializeFireBase() async {
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
 }
